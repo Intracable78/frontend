e802c74391c4bcd255751f8721d79528fc58aa66
intracable78 <matteo.tomasi@ynov.com>
2023-05-10 16:02:26 +0200

    frontend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  stripe: {
+    publishableKey: 'enter_public_key',
+    secretKey: 'private_key'
+  },
+  api: 'http://localhost:3000/'
 };
 
 /*
